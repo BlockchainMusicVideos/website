494bb402402a94238e272f8b9582c1303c1626c4
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-05-14 12:45:01 -0700

    chore: Update Clerk publishable key in client hooks

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,8 +1,8 @@
 import type { HandleClientError } from '@sveltejs/kit';
 import { initializeClerkClient } from 'clerk-sveltekit/client';
-import { PUBLIC_CLERK_PUBLISHABLE_KEY } from '$env/static/public';
+import { CLERK_PUBLISHABLE_KEY } from '$env/static/public';
 
-initializeClerkClient(PUBLIC_CLERK_PUBLISHABLE_KEY, {
+initializeClerkClient(CLERK_PUBLISHABLE_KEY, {
 	afterSignInUrl: '/videos/',
 	afterSignUpUrl: '/videos/',
 	signInUrl: '/signin',
